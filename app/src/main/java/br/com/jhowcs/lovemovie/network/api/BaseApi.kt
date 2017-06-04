3cdd367ab49441e334f51529fcaa9b47a1acb3e4
Jonathan Campos <jhowcs@gmail.com>
2017-06-04 16:25:27 -0300

    Add OkHttpClient to Retrofit
    - Change BaseApi to add OkHttpClient to Retrofit
      with an interceptor to http calls

--- app/src/main/java/br/com/jhowcs/lovemovie/network/api/BaseApi.kt
+++ app/src/main/java/br/com/jhowcs/lovemovie/network/api/BaseApi.kt
@@ -1,5 +1,6 @@
 package br.com.jhowcs.lovemovie.network.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,12 +9,17 @@ class BaseApi private constructor() {
     companion object {
         private var mRetrofit: Retrofit? = null
         private val mBaseUrl: String = "https://api.themoviedb.org/3/"
+        private val mInterceptor: RequestInterceptor = RequestInterceptor()
+        private val mClient: OkHttpClient = OkHttpClient
+                        .Builder()
+                        .addInterceptor(mInterceptor).build()
 
         fun getInstance(): Retrofit {
             if (mRetrofit === null) {
                 mRetrofit = Retrofit.Builder()
                         .baseUrl(mBaseUrl)
                         .addConverterFactory(GsonConverterFactory.create())
+                        .client(mClient)
                         .build()
             }
 
