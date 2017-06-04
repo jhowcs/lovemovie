70ee7cc27685f0151b45f4c56597c04b9a0ca336
Jonathan Campos <jhowcs@gmail.com>
2017-06-04 17:14:21 -0300

    Add getTopRatedMovies to MovieService class
    - Add method to fetch the top rated movies
      from tha API

--- app/src/main/java/br/com/jhowcs/lovemovie/network/service/MovieService.kt
+++ app/src/main/java/br/com/jhowcs/lovemovie/network/service/MovieService.kt
@@ -1,6 +1,6 @@
 package br.com.jhowcs.lovemovie.network.service
 
-import br.com.jhowcs.lovemovie.feature.movie.popular.model.PopularMovies
+import br.com.jhowcs.lovemovie.feature.movie.model.MovieResults
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,5 +10,9 @@ interface MovieService {
 
     @GET("movie/popular")
     fun getPopularMovies(@Query("language") language: String = "pt-BR",
-                         @Query("page") page: Int = 1): Call<PopularMovies>
+                         @Query("page") page: Int = 1): Call<MovieResults>
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(@Query("language") language: String = "pt-BR",
+                         @Query("page") page: Int = 1): Call<MovieResults>
 }
