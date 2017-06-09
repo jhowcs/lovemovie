df7c8e492cc7ac7443376e33eba619896ce24aed
Jonathan Campos <jhowcs@gmail.com>
2017-06-08 22:08:22 -0300

    Change Configuration's file package

--- app/src/main/java/br/com/jhowcs/lovemovie/network/service/ConfigurationService.kt
+++ app/src/main/java/br/com/jhowcs/lovemovie/network/service/ConfigurationService.kt
@@ -1,6 +1,6 @@
 package br.com.jhowcs.lovemovie.network.service
 
-import br.com.jhowcs.lovemovie.feature.configuration.Configuration
+import br.com.jhowcs.lovemovie.feature.configuration.model.Configuration
 import retrofit2.Call
 import retrofit2.http.GET
 
