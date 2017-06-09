70b830aaeb81a44e5c21b004bfcc63d196d6caa8
Jonathan Campos <jhowcs@gmail.com>
2017-06-08 22:13:10 -0300

    Change MovieAdapter
    - Refactor adapter to fill the fields
      image, title, release date and vote

--- app/src/main/java/br/com/jhowcs/lovemovie/feature/movie/MovieAdapter.kt
+++ app/src/main/java/br/com/jhowcs/lovemovie/feature/movie/MovieAdapter.kt
@@ -5,11 +5,16 @@ import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import br.com.jhowcs.lovemovie.R
 import br.com.jhowcs.lovemovie.feature.movie.model.Movie
+import br.com.jhowcs.lovemovie.network.api.ConfigurationApi
+import com.bumptech.glide.Glide
 
-class MovieAdapter(val movieList: Array<Movie>): Adapter<ViewHolder>() {
+class MovieAdapter(val movieList: ArrayList<Movie>): Adapter<ViewHolder>() {
+
+    private val baseImageUrl = ConfigurationApi.mConfiguration.images.baseUrl
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent?.context)
@@ -18,8 +23,18 @@ class MovieAdapter(val movieList: Array<Movie>): Adapter<ViewHolder>() {
 
     override fun onBindViewHolder(viewHolder: ViewHolder?, position: Int) {
         if (viewHolder is MovieVH) {
-            viewHolder.title.text = movieList[position].title
+            val movie = movieList[position]
+            loadPoster(viewHolder.poster, movie.posterPath)
+            viewHolder.title.text = movie.title
+            viewHolder.releaseDate.text = movie.releaseDate
+            viewHolder.voteAvg.text = "vote: ${movie.voteAverage.toString()}"
         }
+    }
+
+    private fun loadPoster(poster: ImageView, posterEndpoint: String) {
+        Glide.with(poster)
+                .load(baseImageUrl.plus("w185/").plus(posterEndpoint))
+                .into(poster)
     }
 
     override fun getItemCount(): Int {
@@ -27,6 +42,9 @@ class MovieAdapter(val movieList: Array<Movie>): Adapter<ViewHolder>() {
     }
 
     private class MovieVH(itemView: View?) : ViewHolder(itemView) {
+        val poster by lazy { itemView?.findViewById(R.id.imgPoster) as ImageView }
         val title by lazy { itemView?.findViewById(R.id.txtMovieTitle) as TextView }
+        val releaseDate by lazy { itemView?.findViewById(R.id.txtReleaseDate) as TextView }
+        val voteAvg by lazy { itemView?.findViewById(R.id.txtVoteAverage) as TextView }
     }
 }
