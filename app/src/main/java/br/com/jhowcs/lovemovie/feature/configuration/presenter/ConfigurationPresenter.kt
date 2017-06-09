c0beecfaa3cc4a093f22931c2c85690bf41502e2
Jonathan Campos <jhowcs@gmail.com>
2017-06-08 22:12:21 -0300

    Add ConfigurationPresenter class

--- app/src/main/java/br/com/jhowcs/lovemovie/feature/configuration/presenter/ConfigurationPresenter.kt
+++ app/src/main/java/br/com/jhowcs/lovemovie/feature/configuration/presenter/ConfigurationPresenter.kt
@@ -1,5 +1,34 @@
 package br.com.jhowcs.lovemovie.feature.configuration.presenter
 
-/**
- * Created by jonathan on 6/8/17.
- */
+import br.com.jhowcs.lovemovie.feature.configuration.model.Configuration
+import br.com.jhowcs.lovemovie.network.api.BaseApi
+import br.com.jhowcs.lovemovie.network.service.ConfigurationService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ConfigurationPresenter {
+
+    interface LoadConfigurationCallback {
+        fun onConfigurationLoaded(configuration: Configuration)
+    }
+
+    fun loadConfiguration(callback: LoadConfigurationCallback) {
+        val retrofit = BaseApi.getInstance()
+        val configurationService = retrofit.create(ConfigurationService::class.java)
+
+        configurationService.getConfiguration().enqueue(object : Callback<Configuration> {
+
+            override fun onResponse(call: Call<Configuration>?, response: Response<Configuration>?) {
+                if (response?.body() !== null) {
+                    val configuration = response.body() as Configuration
+                    callback.onConfigurationLoaded(configuration)
+                }
+            }
+
+            override fun onFailure(call: Call<Configuration>?, t: Throwable?) {
+            }
+        })
+    }
+
+}
